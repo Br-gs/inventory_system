e34a46e1e557a7fefab04168fc87e81bb6a3b6b7
Br-gs <brayan106059@gmail.com>
2024-11-12 21:20:38 -0500

    creation of some urls, views, a template and renaming the value of the classes of the models

--- backend/inventory_management/models.py
+++ backend/inventory_management/models.py
@@ -5,17 +5,17 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    number = models.IntegerField()
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class InventoryMovement(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    number = models.IntegerField()
+    quantity = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=50)  
 
     def __str__(self):
-        return f"{self.product.name} - {self.type} - {self.number}"
+        return f"{self.product.name} - {self.type} - {self.quantity}"
 
