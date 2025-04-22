89f237324eacffa0508b865e19f14f5a12724e99
Br-gs <brayan106059@gmail.com>
2025-04-21 22:14:19 -0500

    Add validators to quantity field and define movement types in InventoryMovement model

--- backend/inventory_management/models.py
+++ backend/inventory_management/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return self.name
@@ -14,8 +16,20 @@ class InventoryMovement(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=50)  
+    MOVEMENT_INPUT = 'IN'
+    MOVEMENT_OUTPUT = 'OUT'
+    MOVEMENT_ADJUSTMENT = 'ADJ'
+    MOVEMENT_TYPE_CHOICES = [
+        (MOVEMENT_INPUT, 'input'),
+        (MOVEMENT_OUTPUT, 'output'),
+        (MOVEMENT_ADJUSTMENT, 'adjusting'),
+    ]
+    movement_type = models.CharField(
+        max_length=3,
+        choices=MOVEMENT_TYPE_CHOICES,
+        default=MOVEMENT_INPUT,
+    )  
 
     def __str__(self):
-        return f"{self.product.name} - {self.type} - {self.quantity}"
+        return f"{self.product.name} - {self.get_movement_type_display()} - {self.quantity}"
 
