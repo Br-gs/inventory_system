8e0902b20353076644948d33ae9e985670cf8cb0
Br-gs <brayan106059@gmail.com>
2025-04-21 22:45:04 -0500

    Refactor serializers to include InventoryMovement and specify fields for ProductSerializer

--- backend/inventory_management/serializers.py
+++ backend/inventory_management/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, InventoryMovement
 
 class ProductSerializer (serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'price', 'quantity']
+
+class InventoryMovementSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    class Meta:
+        model = InventoryMovement
+        fields = ['id', 'product', 'quantity', 'date', 'movement_type', 'date']
+        read_only_fields = ['date']
