fad67200fd317f3fdb072dd7b921ee153fd8c1ca
Br-gs <brayan106059@gmail.com>
2025-07-10 19:16:26 -0500

    feat: add SearchSuggestions component to index exports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import modal from './modal';
 import productForm from './productForm';
 import productList from './productList';
 import MovementList from './MovementList';
+import SearchSuggestions from './SearchSuggestions';
 
-export { modal, productForm, productList, MovementList };
+export { modal, productForm, productList, MovementList, SearchSuggestions }
